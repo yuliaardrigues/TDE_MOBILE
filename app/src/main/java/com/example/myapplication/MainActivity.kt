73273a424415a4b78f592e50f6eeabe2e8aba4ee
yulia <yuliarodrigues@aluno.unifapce.edu.br>
2024-04-15 00:36:30 -0300

    botao chamando a variavel com nome nome

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            var nome = binding.nome.text.toString().trim()
+            binding.result.text = "Ola," + nome
+        }
+
+    }
+}
